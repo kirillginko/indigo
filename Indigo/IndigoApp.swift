d6dee0fd19bc3a4894abf0ff6c856f8167e25cb4
Kirill Ginko <53977030+kirillginko@users.noreply.github.com>
2026-08-27 13:54:15 -0400

    Initial Commit

---
+++ Indigo/IndigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndigoApp.swift
+//  Indigo
+//
+//  Created by Kirill Ginko on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct IndigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
